a433d96fb24b9b6a78feb3d84fc27df2a5eaa9dd
Darko Lukic <lukicdarkoo@gmail.com>
2019-11-16 02:59:09 +0000

    Add scheduled temperature read

--- src/growbothub_tlc/src/scheduler.py
+++ src/growbothub_tlc/src/scheduler.py
@@ -5,8 +5,19 @@ import time
 import json
 import rospy
 import os
+import sys
 from datetime import datetime
 from growbothub_tlc.srv import DeviceReadWrite
+
+
+if sys.version_info[0] < 3 or sys.version_info[1] < 4:
+    # python version < 3.3
+    import time
+    def timestamp(date):
+        return time.mktime(date.timetuple())
+else:
+    def timestamp(date):
+        return date.timestamp()
 
 
 device_read = None
@@ -27,10 +38,11 @@ def take_picture():
 
 
 def meassure_temperature():
-    timestamp = datetime.now().timestamp()
-    params = json.loads(device_read('temp', ''))
+    epoch = timestamp(datetime.now())
+    readings = device_read('temp', '').readings
+    params = json.loads(readings)
     with open(os.path.join(DIST_DATA, 'temp.csv'), 'a') as f:
-        f.write('{},{},{}\n'.format(timestamp, params['temperature'], params['humidity']))
+        f.write('{},{},{}\n'.format(epoch, params['temperature'], params['humidity']))
 
 
 
@@ -39,7 +51,7 @@ if __name__ == "__main__":
     r = rospy.Rate(1)
 
     schedule.every(60).minutes.do(take_picture) 
-    schedule.every(30).minutes.do(meassure_temperature) 
+    schedule.every(1).minutes.do(meassure_temperature) 
     schedule.every().day.at('07:00').do(turn_lights_on) 
     schedule.every().day.at('20:00').do(turn_lights_off) 
 
