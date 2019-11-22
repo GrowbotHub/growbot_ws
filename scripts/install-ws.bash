da82295b91aa5f708d94da76d811465cce517c9a
Darko Lukić <lukicdarkoo@gmail.com>
2019-11-22 16:14:43 +0100

    Update install-ws.bash

--- scripts/install-ws.bash
+++ scripts/install-ws.bash
@@ -3,13 +3,13 @@ sudo apt-get install -y python-rosdep python-rosinstall-generator python-wstool 
 sudo rosdep init
 rosdep update
 
-# Add internal and external dependencies
+# Add internal and non-published dependencies
 rosinstall_generator ros_comm --rosdistro melodic --deps --wet-only --tar > melodic-ros_comm-wet.rosinstall
 wstool init -j3 ./src melodic-ros_comm-wet.rosinstall
-rosdep install -y --from-paths ./src --ignore-src --rosdistro melodic -r --os=debian:buster
-
-# Add non-published packages
 git clone --depth=1 https://github.com/RobotWebTools/rosbridge_suite.git src/rosbridge_suite
+
+# Resolve external dependencies
+rosdep install -y --from-paths ./src --ignore-src --rosdistro melodic -r --os=debian:buster
 
 # Configure catkin
 sudo ./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/melodic -j1
